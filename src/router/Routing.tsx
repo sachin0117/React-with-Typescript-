0c2d719e799622dd2e61750bbcd4a6617a98ec60
Sachin Sharma <sharmasachinwins@gmail.com>
2025-06-25 16:52:04 +0530

    Preview Data functionality

--- src/router/Routing.tsx
+++ src/router/Routing.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Signin from "../pages/signin/Signin";
 import Signup from "../pages/signup/Signup";
-import Dashboard, { GetUserDetailloader } from "../layout/dashboard/Dashboard";
+import Dashboard, { GetUserDetailLoader } from "../layout/dashboard/Dashboard";
 import Cart from "../pages/cartdetail/Cart";
 import NotFoundPage from "../pages/error/NotFound";
 
@@ -11,7 +11,7 @@ export default function Routes() {
             <>
                 <Route path="/" element={<Signin />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/dashboard" element={<Dashboard />} loader={GetUserDetailloader} />
+                <Route path="/dashboard" element={<Dashboard />} loader={GetUserDetailLoader} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="*" element={<NotFoundPage />} />
             </>
